225a719bccf2629ed1ed8f8495e43d5b46adb88d
dev <dev@jays-MBP.lan>
2024-07-15 13:41:25 -0700

    rendered a list of projects from a json file and created a search filter input

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
+import DATA from './data.json';
 
 function App() {
+  const [search, setSearch] = useState("");
+
   return (
     <div>
-      hi
-      hi
+      <input
+        type='text'
+        placeholder='search...'
+        value={ search }
+        onChange={(e) => setSearch(e.target.value)}
+      />
+
+      <div>
+        <ul>
+          {DATA.filter((item) =>
+            item.title.toLowerCase().includes(search.toLowerCase())
+          ).map((data, index) => (
+            <li key={index}>{data.title}</li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
